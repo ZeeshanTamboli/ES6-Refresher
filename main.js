20cc25cad860d3f9bf4451108bce6b4ce2a020e1
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-09-28 11:50:51 +0530

    Classes and Subclasses

--- main.js
+++ main.js
@@ -13,10 +13,9 @@ const newPerson = {
   email: 'zee@gmail.com'
 };
 
-console.log(newPerson);
+// console.log(newPerson);
 
 // DESTRUCTURING
-
 const profile = {
   name: 'Zeeshan Tamboli',
   address: {
@@ -28,4 +27,37 @@ const profile = {
 
 const { name, address, hobbies } = profile;
 
-console.log(name, address, hobbies[0]);
+// console.log(name, address, hobbies[0]);
+
+// CLASSES
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    return `Hello, my name is ${this.name} and I am ${this.age}`;
+  }
+}
+
+const person1 = new Person('John', 33);
+const person2 = new Person('Jack', 28);
+
+// console.log(person2.greet());
+
+// SUBCLASSES
+class Customer extends Person {
+  constructor(name, age, balance) {
+    super(name, age);
+    this.balance = balance;
+  }
+
+  info() {
+    return `${this.name} owes ₹${this.balance}`;
+  }
+}
+
+const customer1 = new Customer('Kevin', 32, 300);
+
+console.log(customer1.info());
